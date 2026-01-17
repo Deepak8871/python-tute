c6520ccfb75479db5df1ce549f3262bc53957ce3
Deepak8871 <139011150+Deepak8871@users.noreply.github.com>
2026-01-18 02:09:14 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,38 @@
+# Task 1 : Basic Mathematical Operations
+
+# 1. Assign two numbers
+num1 = 5
+num2 = 10
+
+# 2. Perform basic mathematical operations
+
+# 2.1 Addition
+addition = num1 + num2
+
+# 2.2 Subtraction
+subtraction = num1 - num2
+
+# 2.3 Multiplication
+multiplication = num1 * num2
+
+# 2.4 Division
+division = num1 / num2
+
+# 3. Display the results
+print("Results:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+
+# Task 2: Create a Personalized Greeting
+
+# 1. Assign first name and last name
+first_name = "Deepak"
+last_name = "kumar"
+
+# 2. Concatenate into full name
+full_name = first_name + " " + last_name
+
+# 3. Print personalized greeting
